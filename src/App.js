e13c36ba503bf348ee849a15fe7439a698d4de99
Prijesh M <prijesh1987@gmail.com>
2019-07-25 23:00:41 +0530

    onClick execute fn with params

--- src/App.js
+++ src/App.js
@@ -16,24 +16,29 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>It's working fine</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-        {/* <Person name="Jijina" age="31" />
-        <Person name="Aadhith" age="5">
-          My Hobby: Coloring
-        </Person> */}
+        <button onClick={() => this.switchNameHandler('Jijina Prijesh')}>Switch Name</button>
+        <Person
+          click={this.switchNameHandler.bind(this, 'Priju')}
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} />
+        <Person 
+          click={() => this.switchNameHandler('Aadhith M')}
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
 
-  switchNameHandler = event => {
+  switchNameHandler = newName => {
     console.log("Clicked!!!");
+    newName = newName ? newName : '';
     // DON'T DO THIS: this.state.persons[0].name = 'Prijesh Meppayil';
     this.setState({
       persons: [
-        { name: 'Prijesh Meppayil', age: 32 },
+        { name: newName, age: 32 },
         { name: 'Jijina', age: 31 },
         { name: 'Aadhith', age: 5 },
       ]
